3e14fd619df462e4156562d1de7fc685fe196b04
RomuCampu <romucampu@gmail.com>
2018-10-20 23:19:39 +0200

    Fetching bokkmars function

--- Bookmaker/js/main.js
+++ Bookmaker/js/main.js
@@ -21,7 +21,7 @@ function saveBookmark(e) {
         localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
     } else {
         // Get bookmarks from local storage
-        var bookmakrs = JSON.parse(localStorage.getItem('bookmarks'));
+        var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
         // Add bookmark to array //
         bookmarks.push(bookmark);
         // Back to localStorage
@@ -30,7 +30,22 @@ function saveBookmark(e) {
     }
 
 
-    //console.log(siteName);
     // Prevents from submitting //
     e.preventDefault();
+}
+
+// Fetch bookmarks //
+function fetchBookmarks() {
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    var bookmarksResults = document.getElementById('bookmarksResults');;
+
+    bookmarksResults.innerHTML = '';
+
+    for (var i = 0; i < bookmarks.length; i++) {
+        var name = bookmarks[i].name;
+        var url = bookmarks[i].url;
+
+        bookmarksResults.innerHTML += '<div class="well">' +
+            '<h3>' + name + '</h3>' + '</div>';
+    }
 }
